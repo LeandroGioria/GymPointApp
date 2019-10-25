565b048d55c465afb5866dad07b055064146068c
Leandro Gioria <legioria@hotmail.com>
2019-10-24 21:46:18 -0300

    Created new routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
@@ -9,5 +11,10 @@ routes.get('/', (req, res) => {
 });
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// Only the routes below will use the middleware auth
+routes.use(authMiddleware);
+routes.post('/students', StudentController.store);
+routes.put('/students', StudentController.update);
 
 export default routes;
