645b71d627f72969fd6994f2a8841ab801f04663
Leandro Gioria <legioria@hotmail.com>
2019-12-04 00:07:04 -0300

    Created frond-end application structure and configured eslint, prettier

---
+++ front-end/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+		'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  parser: 'babel-eslint',
+  plugins: [
+    'react',
+    'prettier',
+    'react-hooks',
+    'import',
+    'jsx-a11y'
+  ],
+  rules: {
+        "prettier/prettier": "error",
+        "react/jsx-filename-extension": ['error', { extensions: ['.jsx', '.js'] }],
+        "import/prefer-default-export": 'off',
+        "no-unused-vars":["error", { argsIgnorePattern:"^_" }],
+        "react/jsx-one-expression-per-line": "off",
+        "global-require": "off",
+        "react-native/no-raw-text": "off",
+        "no-param-reassign": "off",
+        "no-underscore-dangle": "off",
+        camelcase: "off",
+        "no-console": ["error", { allow: ["tron"] }],
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+  },
+};
