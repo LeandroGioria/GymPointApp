779a690f516c8a87530878071fdd095ec5b8daca
Leandro Gioria <legioria@hotmail.com>
2019-12-31 02:09:29 -0300

    Styled help list page

--- mobile/GymPointMobile/src/pages/HelpOrderList/index.js
+++ mobile/GymPointMobile/src/pages/HelpOrderList/index.js
@@ -1,8 +1,74 @@
 import React from 'react';
-import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
+import Header from '~/components/Header';
+
+import {
+  Container,
+  NewHelpButton,
+  HelpList,
+  Help,
+  Title,
+  Time,
+  Question,
+  HelpHeader,
+} from './styles';
 
 export default function HelpOrderList() {
-  return <Text>HelpOrderList</Text>;
+  return (
+    <Background>
+      <Container>
+        <Header />
+        <NewHelpButton>Novo pedido de auxílio</NewHelpButton>
+        <HelpList>
+          <Help>
+            <HelpHeader>
+              <Title feedback>Respondido</Title>
+              <Time>Hoje ás 14h</Time>
+            </HelpHeader>
+            <Question>
+              Olá pessoal da academia, gostaria de saber se quando acordar devo
+              ingerir batata doce e frango logo de primeira, preparar asOlá
+              pessoal da academia, gostaria de saber se quando acordar devo
+              ingerir batata doce e frango logo de primeira, preparar asOlá
+              pessoal da academia, gostaria de saber se quando acordar devo
+              ingerir batata doce e frango logo de primeira, preparar asOlá
+              pessoal da academia, gostaria de saber se quando acordar devo
+              ingerir batata doce e frango logo de primeira, preparar as
+            </Question>
+          </Help>
+          <Help>
+            <HelpHeader>
+              <Title>{0 ? 'Respondido' : 'Sem resposta'}</Title>
+              <Time>Hoje ás 14h</Time>
+            </HelpHeader>
+            <Question>
+              Olá pessoal da academia, gostaria de saber se quando acordar devo
+              ingerir batata doce e frango logo de primeira, preparar as
+            </Question>
+          </Help>
+          <Help>
+            <HelpHeader>
+              <Title>{0 ? 'Respondido' : 'Sem resposta'}</Title>
+              <Time>Hoje ás 14h</Time>
+            </HelpHeader>
+            <Question>
+              Olá pessoal da academia, gostaria de saber se quando acordar devo
+              ingerir batata doce e frango logo de primeira, preparar as
+            </Question>
+          </Help>
+          <Help>
+            <HelpHeader>
+              <Title>{0 ? 'Respondido' : 'Sem resposta'}</Title>
+              <Time>Hoje ás 14h</Time>
+            </HelpHeader>
+            <Question>
+              Olá pessoal da academia, gostaria de saber se quando acordar devo
+              ingerir batata doce e frango logo de primeira, preparar as
+            </Question>
+          </Help>
+        </HelpList>
+      </Container>
+    </Background>
+  );
 }
--- mobile/GymPointMobile/src/pages/HelpOrderList/styles.js
+++ mobile/GymPointMobile/src/pages/HelpOrderList/styles.js
@@ -1,3 +1,51 @@
+import { Platform } from 'react-native';
+
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import Button from '~/components/Button';
+
+export const Container = styled.KeyboardAvoidingView.attrs({
+  enabled: Platform.OS === 'ios',
+  behavior: 'padding',
+})`
+  flex: 1;
+  align-items: center;
+`;
+
+export const NewHelpButton = styled(Button)`
+  width: 335px;
+  margin-top: 20px;
+`;
+
+export const HelpList = styled.ScrollView.attrs({
+  // Change to flatlist
+  showsVerticalScrollIndicator: false,
+})``;
+
+export const Help = styled.View`
+  background: #fff;
+  width: 335px;
+  height: 150px;
+  border-radius: 4px;
+  border: solid 1px #dddddd;
+  padding: 20px 20px;
+  margin: 10px 0;
+`;
+
+export const HelpHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+export const Title = styled.Text`
+  color: ${props => (props.feedback ? '#42CB59' : '#999')};
+`;
+
+export const Time = styled.Text.attrs()`
+  color: #666;
+`;
+
+export const Question = styled.Text`
+  color: #666;
+`;
